e40983144d5c2f347f177ccf0d9800ffa71969ae
NAGALAKSHMI M <nagalakshmi052005@gmail.com>
2025-11-13 11:52:45 +0530

    Update Firebase.js

--- src/assets/Firebase/Firebase.js
+++ src/assets/Firebase/Firebase.js
@@ -1,19 +1,9 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyACyGGcVqaMlpo9BXQinr4FjJbHxPeeH78",
-  authDomain: "skillsweb-8d262.firebaseapp.com",
-  projectId: "skillsweb-8d262",
-  storageBucket: "skillsweb-8d262.firebasestorage.app",
-  messagingSenderId: "1011668610753",
-  appId: "1:1011668610753:web:086ae6bd59334306bf6e40",
-  measurementId: "G-603HZG3CS8"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
